1980fe6a7f62c25e5caf70ae1d7c0e4ba2108b00
KS-Arafat <sfnarafat@gmail.com>
2024-01-10 19:51:26 +0600

    Course Selector page done

--- app/(frontend)/coursefinder/CourseField.tsx
+++ app/(frontend)/coursefinder/CourseField.tsx
@@ -1,59 +1,81 @@
 "use client";
-import courses from "@/utils/getCourse";
-import React, { useState } from "react";
+import Image from "next/image";
+import { useState, useEffect, Suspense } from "react";
 
-const CourseField = () => {
-	const [inputValue, setInputValue] = useState("");
-	const [suggestions, setSuggestions] = useState([""]);
+const CourseField = ({ className }: { className?: string }) => {
+  const [inputValue, setInputValue] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
 
-	const handleInputChange = (e: any) => {
-		const value = e.target.value;
-		setInputValue(value);
+  useEffect(() => {
+    const fetchSuggestions = async () => {
+      if (inputValue.length > 7) return;
+      try {
+        const response = await fetch("/api/getCrs", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ characters: inputValue }),
+        });
 
-		if (value == "") {
-			setSuggestions([]);
-			return;
-		}
-		let filteredSuggestions: Array<string> = [];
-		let count = 0;
-		for (let suggestion of courses) {
-			if (
-				count < 20 &&
-				suggestion.toLowerCase().startsWith(value.toLowerCase())
-			) {
-				filteredSuggestions.push(suggestion);
-				count++;
-			}
-		}
-		// const filteredSuggestions = hardcodedSuggestions.filter((suggestion) =>
-		// 	suggestion.toLowerCase().includes(value.toLowerCase())
-		// );
+        if (!response.ok) {
+          throw new Error("Failed to fetch suggestions");
+        }
 
-		setSuggestions(filteredSuggestions);
-	};
+        const data = await response.json();
+        console.log(data);
 
-	const handleSuggestionClick = (suggestion: string) => {
-		setInputValue(suggestion);
-		setSuggestions([]); // Clear suggestions after clicking on a suggestion
-	};
+        setSuggestions(data.suggestions);
+      } catch (error) {
+        console.error("Error fetching suggestions:", error);
+      }
+    };
 
-	return (
-		<div>
-			<input
-				type="text"
-				value={inputValue}
-				onChange={handleInputChange}
-				placeholder="Type here..."
-			/>
-			<ul>
-				{suggestions.map((suggestion, index) => (
-					<li key={index} onClick={() => handleSuggestionClick(suggestion)}>
-						{suggestion}
-					</li>
-				))}
-			</ul>
-		</div>
-	);
+    fetchSuggestions();
+  }, [inputValue]);
+
+  const handleInputChange = async (e: any) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <div className={className}>
+      <div className="w-32">
+        <div className="relative">
+          <input
+            className="row-start-1 row-end-2 max-h-10 w-32 gap-0 rounded-md p-2 pr-8 outline-none ring-4 transition-all duration-1000 ease-in-out focus:ring-inset focus:ring-[#89CFF3]"
+            type="text"
+            id="characterType"
+            value={inputValue}
+            onChange={handleInputChange}
+            name="selectedCourse"
+          />
+          <Image
+            className="absolute right-2 top-2.5 m-0 h-5 w-5 rounded-full p-0 active:border-4 active:border-red-400"
+            draggable="false"
+            src="cross-square.svg"
+            height="20"
+            width="20"
+            alt=""
+            onClick={() => setInputValue("")}
+          />
+        </div>
+        <div className="grid w-32 grid-cols-1 rounded-md bg-slate-300 p-1 text-center">
+          <ul className="transition-all duration-500">
+            {suggestions.map((suggestion, index) => (
+              <li
+                className="cursor-pointer rounded-md border-b-2 border-cyan-200 pb-1 duration-300 hover:bg-cyan-200 hover:font-bold active:bg-cyan-200 active:font-bold"
+                key={index}
+                onClick={() => setInputValue(suggestion)}
+              >
+                {suggestion}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CourseField;
--- app/(frontend)/coursefinder/page.tsx
+++ app/(frontend)/coursefinder/page.tsx
@@ -1,9 +1,128 @@
+import Image from "next/image";
 import CourseField from "./CourseField";
+import Testanimation from "./testanimation";
+import { cookies } from "next/headers";
+import coursesList from "@/utils/getCourseList";
+import { redirect } from "next/navigation";
 
-export default function coursefinder() {
-	return (
-		<div className="grid grid-cols-1 place-items-center">
-			<CourseField />
-		</div>
-	);
+const cookieName = "coursetaken";
+
+const AddCourseAction = async (data: FormData) => {
+  "use server";
+  let takencourse: string[] = [];
+  let newCourse = data.get("selectedCourse")?.toString().toLocaleUpperCase();
+  if (
+    newCourse == "" ||
+    newCourse == undefined ||
+    !coursesList.includes(newCourse)
+  )
+    return;
+  const cookie = cookies();
+  const rawCookie = cookie.get(cookieName);
+  try {
+    if (rawCookie == undefined || rawCookie.value == "") {
+      cookie.set(cookieName, JSON.stringify([]));
+      takencourse = [];
+    } else takencourse = JSON.parse(rawCookie.value) as string[];
+  } catch (error) {
+    cookie.set(cookieName, JSON.stringify([]), {
+      maxAge: 60 * 60,
+    });
+  }
+
+  if (takencourse.includes(newCourse) || takencourse.length > 5) return;
+
+  takencourse.push(newCourse);
+
+  cookie.set(cookieName, JSON.stringify(takencourse));
+  return takencourse;
+};
+
+export default function coursefinder({
+  searchParams,
+}: {
+  searchParams: { msg: string };
+}) {
+  const cookie = cookies();
+  const rawCookie = cookie.get(cookieName);
+  let takencourse: string[] = [];
+  try {
+    if (rawCookie == undefined || rawCookie.value == "") {
+      takencourse = [];
+    } else takencourse = JSON.parse(rawCookie.value) as string[];
+  } catch (error) {
+    cookie.set(cookieName, JSON.stringify([]));
+  }
+  console.log(takencourse);
+
+  return (
+    <div className="flex flex-col items-center overflow-visible">
+      <p className="w-full animate-pulse text-pretty rounded-full border-b-2 border-double border-[#89CFF3] p-3 text-center text-3xl font-bold text-[#00A9FF]">
+        Course Watcher
+      </p>
+      <div className="m-1 grid h-screen w-11/12 grid-cols-2 p-1 pt-5 sm:w-9/12 sm:gap-10">
+        <form action={AddCourseAction} className="relative w-full ">
+          <CourseField className="sm:absolute sm:right-[5rem] sm:top-4" />
+          <button
+            type="submit"
+            className="fixed bottom-10 right-10 z-10 m-0 h-fit max-w-32  bg-slate-200 p-0 sm:absolute sm:right-5 sm:top-4 sm:max-w-10"
+          >
+            <Image
+              className="active:max-w-24 active:border-4 active:blur-sm sm:active:max-w-10 sm:active:border-2"
+              draggable="false"
+              src="plus-square.svg"
+              width="60"
+              height="60"
+              alt=""
+            />
+          </button>
+        </form>
+        <div className="card ] relative grid h-fit w-full grid-cols-1 justify-items-end rounded-lg bg-[#b4ecfd] shadow-xl">
+          <form className="h-[50vh] w-full rounded-lg p-3">
+            <p className="mb-4 rounded-xl border border-b-4 border-l-2 border-double border-[#2647ff] p-2 text-center font-serif font-semibold uppercase text-[#00aaff] shadow-md">
+              Added Courses
+            </p>
+            <div className="h-9/12 mx-2">
+              <ul className="grid h-full cursor-pointer grid-cols-1 gap-3 px-2 sm:grid-cols-2">
+                {takencourse.map((e) => (
+                  <li
+                    className="custom-li rounded-lg py-1 text-center"
+                    key={Math.random()}
+                  >
+                    {e}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="custom-buttons absolute -bottom-14 left-0 grid w-full grid-cols-2">
+              <button
+                className="custom-button m-1"
+                type="submit"
+                formAction={async () => {
+                  "use server";
+                  redirect("/watch");
+                }}
+              >
+                Watch
+              </button>
+              <button
+                className="custom-button m-1 "
+                type="submit"
+                formAction={async () => {
+                  "use server";
+                  cookies().delete(cookieName);
+                }}
+              >
+                Clear
+              </button>
+            </div>
+          </form>
+          <span className="top"></span>
+          <span className="right"></span>
+          <span className="bottom"></span>
+          <span className="left"></span>
+        </div>
+      </div>
+    </div>
+  );
 }
